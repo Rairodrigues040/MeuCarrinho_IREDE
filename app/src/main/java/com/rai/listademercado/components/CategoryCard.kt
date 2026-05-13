3dfa0de5abec9f0c70d40c1de50b152176a17728
Rai <rairodrigues@alu.ufc.br>
2026-05-13 14:38:48 -0300

    Atualização v3

--- app/src/main/java/com/rai/listademercado/components/CategoryCard.kt
+++ app/src/main/java/com/rai/listademercado/components/CategoryCard.kt
@@ -1,18 +1,45 @@
 package com.rai.listademercado.components
 
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.rai.listademercado.Category
+import com.rai.listademercado.ui.theme.BrownPrimary
 
 @Composable
 fun CategoryCard(
@@ -20,9 +47,10 @@ fun CategoryCard(
     expanded: Boolean,
     onToggle: () -> Unit,
     modifier: Modifier = Modifier,
-//    content: @Composable () -> Unit
+    content: @Composable () -> Unit
 ){
     val shape = RoundedCornerShape(25.dp)
+
     Surface(
         shape = shape,
         color = category.backgroudColor,
@@ -30,22 +58,74 @@ fun CategoryCard(
             .fillMaxWidth()
             .border(1.dp, category.mainColor)
             .clip(shape)
+            .clickable{onToggle()}
+            .animateContentSize()
     ) {
-        Column() {
+        Column(
+            modifier = Modifier.padding(20.dp)
+        ) {
             Row(
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
-            ) { }
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+                        .background(category.mainColor),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        painter = painterResource(category.iconRes),
+                        contentDescription = category.title,
+                        tint = Color.White,
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+
+                Spacer(Modifier.width(10.dp))
+
+                Text(
+                    category.title,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.Black
+                )
+
+                Spacer(Modifier.weight(1f))
+
+                Icon(
+                    imageVector = if (expanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                    contentDescription = null,
+                    tint = Color.Black
+                )
+            }
+            AnimatedVisibility(visible = expanded) {
+                Column {
+                    Spacer(Modifier.height(15.dp))
+                    content()
+                }
+            }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 @Composable
 fun PreviewCategoryCard() {
-    CategoryCard(
-        category = Category.FRUITS,
-        expanded = false,
-        onToggle = {},
-    )
+    Surface(color = Color.White) {
+
+        var expanded by remember { mutableStateOf(true) }
+        
+        CategoryCard(
+            category = Category.FRUITS,
+            expanded = expanded,
+            onToggle = { !expanded },
+            modifier = Modifier.padding(18.dp),
+        ) {
+            Column() { }
+        }
+    }
+
 }
